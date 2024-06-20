603a5972cbd2540d9578ede218b4aa02b2748b23
66309010053aom <66309010053@cmtc.ac.th>
2024-06-20 13:55:32 +0700

    new commit

--- app/about/page.js
+++ app/about/page.js
@@ -12,8 +12,7 @@ export default function about() {
                         alt="Card Image"></img>
                     <div class="card-body text-center">
                         <h5 class="card-title">เกมแนะนำ</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <p class="card-text">ว่าไนดำ่ด่่ดร่เ่ว่าไงสุดหล่อบาดว่าไงสุดหล่อ</p>
                         <a href="#" class="btn btn-primary">Button</a>
                     </div>
                 </div>
